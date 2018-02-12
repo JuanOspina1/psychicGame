2ae5884fbbdb0827af77a1cf591c6c23ba20ccb6
Juan Ospina <juanospina11@gmail.com>
2018-02-12 18:00:02 -0500

    cleaned up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-//Letter choices available
+
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Setting all to zero
+
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -11,13 +11,11 @@ var letterToGuess = null;
 
 
 
-//Lets the computer select a random letter from the available choices
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-//Allows the user 9 guesses
-// guesses = guesses || 9
+
 var updateGuessesLeft = function() {
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+
   document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
@@ -25,10 +23,10 @@ var updateLetterToGuess = function() {
   this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
 var updateGuessesSoFar = function() {
-  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+ 
   document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
-// Function will be called when we reset everything
+
 var reset = function() {
   totalGuesses = 9;
   guessesLeft = 9;
@@ -43,7 +41,7 @@ updateLetterToGuess();
 updateGuessesLeft();
 
 
-//When key is released it becomes the users guess
+
 document.onkeyup = function(event) {
     guessesLeft--;
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
@@ -56,15 +54,15 @@ document.onkeyup = function(event) {
             if (userGuess == letterToGuess){
                 wins++;
                 document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("Yes, you are psychic!");
+                alert("Woah, you're psychic!");
                 reset();
             }
         }else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
+
             losses++;
             document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Sorry, you're not psychic, maybe try again?");
-            // Then we'll call the reset. 
+            alert("Keep trying, you have it in you!");
+
             reset();
         }
 };
